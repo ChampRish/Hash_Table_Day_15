f3610572c4083f5e7b54601225037335ec98e894
RISHABH <gguptarishabh82@gmail.com>
2022-03-23 15:41:59 +0530

    Create_MyMapNode_With_Key_Value_Pair

---
+++ UC3_KeyValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable_Day15_Assignment_
+{
+    public class UC3_KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+}
